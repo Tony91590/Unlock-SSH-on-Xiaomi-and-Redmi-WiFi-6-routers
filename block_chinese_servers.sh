768a27438f05fc244bfc0710607436a8e8decdd7
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2023-02-21 00:52:59 +0100

    Update block_chinese_servers.sh

--- block_chinese_servers.sh
+++ block_chinese_servers.sh
@@ -2,10 +2,6 @@
 
 cat << 'EOF' >> /etc/hosts
 
-::1     localhost ip6-localhost ip6-loopback
-ff02::1 ip6-allnodes
-ff02::2 ip6-allrouters
-
 0.0.0.0 baidu.com taobao.com xiaomi.cn xiaomi.com
 0.0.0.0 www.baidu.com www.taobao.com www.xiaomi.cn www.xiaomi.com
 ::1     baidu.com taobao.com xiaomi.cn xiaomi.com
