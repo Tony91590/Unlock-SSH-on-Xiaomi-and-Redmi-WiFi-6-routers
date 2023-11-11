07c18235fce388d1decfe4e2015c274d6906d8ea
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2023-11-11 01:24:12 +0100

    Update block_chinese_servers.sh

--- block_chinese_servers.sh
+++ block_chinese_servers.sh
@@ -10,8 +10,8 @@ ff02::2 ip6-allrouters
 0.0.0.0 www.baidu.com www.taobao.com www.xiaomi.cn www.xiaomi.com
 ::1     baidu.com taobao.com xiaomi.cn xiaomi.com
 ::1     www.baidu.com www.taobao.com www.xiaomi.cn www.xiaomi.com
-0.0.0.0 miwifi.com log.miwifi.com s.miwifi.com app.miwifi.com stun.miwifi.com broker.miwifi.com
-::1     miwifi.com log.miwifi.com s.miwifi.com app.miwifi.com stun.miwifi.com broker.miwifi.com
+0.0.0.0 www.miwifi.com miwifi.com log.miwifi.com s.miwifi.com app.miwifi.com stun.miwifi.com broker.miwifi.com
+::1     www.miwifi.com miwifi.com log.miwifi.com s.miwifi.com app.miwifi.com stun.miwifi.com broker.miwifi.com
 0.0.0.0 bbs.xiaomi.cn bbs.xiaomi.com
 ::1     bbs.xiaomi.cn bbs.xiaomi.com
 EOF
